c16fe687adae147acabcb259f3be4b0c2aa0bb78
kajal <kajalparshdigital111@gmail.com>
2026-06-28 19:15:21 +0530

    fix docfile and cors.php

--- config/cors.php
+++ config/cors.php
@@ -14,13 +14,17 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://mountrise.in',
+        'https://www.mountrise.in',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
+
 ];
